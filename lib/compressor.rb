9085bf3b2b8e3ddc7d13df7ffc280d255e9ac821
Sean Huber <seanhuber@seanhuber.com>
2017-02-26 12:19:24 -0600

    check for the existance of the input video directory

--- lib/compressor.rb
+++ lib/compressor.rb
@@ -1,5 +1,6 @@
 class Compressor
   def self.compress input_dir, output_dir
+    raise ArgumentError, "Unable to locate video directory: #{input_dir}" unless File.directory?(input_dir)
     ap [input_dir, output_dir]
   end
 end
